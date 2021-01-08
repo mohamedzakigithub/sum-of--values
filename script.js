963fa7472c267df010c59c55a74cc13d24632180
Mohamed Eleraky <mohamedzaki@hotmail.com>
2021-01-08 16:36:39 +1100

    edit code and include regular expression

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // Variables declarations and DOM elements selection.
-var inputValuesEl = document.querySelector("#input");
-var outputEl = document.querySelector("#result");
-var buttonEl = document.querySelector("#button");
+const inputValuesEl = document.querySelector("#input");
+const outputEl = document.querySelector("#result");
+const buttonEl = document.querySelector("#button");
 
 // Add click listener to button
 buttonEl.addEventListener("click", function () {
-  // split input values on commas and store in an array.
-  inputArray = inputValuesEl.value.split(",");
+  const inputArray = inputValuesEl.value;
   // get results and assign to output element
-  result = sum(inputArray);
+  let result = sum(inputArray);
   outputEl.innerText = result;
 });
 
 // Function sum declaration
 function sum(arr) {
-  // parse input values to floating point, remove single quotes if present and check if value is a number
-  parsedInputArray = arr.map((el) =>
-    isNaN(parseFloat(el.replace("'", ""))) === false
-      ? parseFloat(el.replace("'", ""))
-      : 0
+  // use a regular expression to remove non digit characters then split input values on commas and store in an array.
+  const digitsArray = arr.replace(/[^0-9\.\,\-]/g, "").split(",");
+  // parse array elements into floats and check if type is a number
+  const parsedArray = digitsArray.map((el) =>
+    isNaN(parseFloat(el)) === false ? parseFloat(el) : 0
   );
   // return the sum of the array using the reduce method
-  return Number(parsedInputArray.reduce((a, b) => a + b, 0).toFixed(2));
+  return Number(parsedArray.reduce((a, b) => a + b, 0).toFixed(2));
 }
